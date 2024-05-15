0c2f7acc657129f80d243533d4bb59f620869a27
obaraelijah <elijahobara357@gmail.com>
2024-05-15 19:37:20 +0300

    refactor: Remove unused imports and fix Diesel trait imports

--- src/protocol/model.rs
+++ src/protocol/model.rs
@@ -1,8 +1,26 @@
 /// Basic models
+/// #[cfg(feature = "backend")]
+use crate::schema::sessions;
 use serde::{Deserialize, Serialize};
+use std::convert::From;
+use diesel::{Insertable, Queryable};
 
+#[cfg_attr(feature = "backend", derive(Insertable, Queryable))]
+#[cfg_attr(feature = "backend", table_name = "sessions")]
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Session {
-    /// The actual session token
+    ///  session token
     pub token: String,
+}
+
+impl Session {
+    /// Create a new session from a given token
+    pub fn new<T>(token: T) -> Self
+    where
+        String: From<T>,
+    {
+        Self {
+            token: token.into(),
+        }
+    }
 }

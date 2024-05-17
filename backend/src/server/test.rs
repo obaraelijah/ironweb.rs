c384d24afd7963b919495d27286257eaaec2e705
obaraelijah <elijahobara357@gmail.com>
2024-05-17 15:10:36 +0300

    test: TlLS server tests

--- backend/src/server/test.rs
+++ backend/src/server/test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 use crate::server::Server;
 use anyhow::{Ok, Result};
+use std::path::PathBuf;
 use webapp::{config::Config, CONFIG_FILENAME};
 
 fn get_config() -> Result<Config> {
@@ -23,6 +24,32 @@ fn fail_to_create_a_server_with_wrong_url() -> Result<()> {
     config.server.url = "".to_owned();
 
     // when
+    // Then
+    assert!(Server::from_config(&config).is_err());
+    Ok(())
+}
+
+#[test]
+fn succeed_to_create_a_server_with_tls() -> Result<()> {
+    // Given
+    let mut config = get_config()?;
+    config.server.url = "https://localhost:30081".to_owned();
+
+    // When
+    // Then
+    assert!(Server::from_config(&config).is_ok());
+    Ok(())
+}
+
+#[test]
+fn fail_to_create_a_server_with_tls_if_not_found() -> Result<()> {
+    // Given
+    let mut config = get_config()?;
+    config.server.url = "https://localhost:30082".to_owned();
+    config.server.cert = PathBuf::new();
+    config.server.key = PathBuf::new();
+
+    // When
     // Then
     assert!(Server::from_config(&config).is_err());
     Ok(())
